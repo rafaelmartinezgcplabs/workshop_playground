4749ad7a2457b2eecc9f64d56dbd6db083a4cfa6
bitsydarel <bitsydarel@gmail.com>
2019-10-12 15:12:52 +0300

    Delayed driver connection by 60 seconds

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,7 +1,6 @@
 // Imports the Flutter Driver API.
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
-
+import "package:flutter_driver/flutter_driver.dart";
+import "package:test/test.dart";
 
 void main() {
   group('Counter App', () {
@@ -14,14 +13,19 @@ void main() {
     FlutterDriver driver;
 
     // Connect to the Flutter driver before running any tests.
-    setUpAll(() async {
-      driver = await FlutterDriver.connect();
-    });
+    setUpAll(
+      () async {
+        driver = await Future<FlutterDriver>.delayed(
+          const Duration(seconds: 60),
+          FlutterDriver.connect,
+        );
+      },
+    );
 
     // Close the connection to the driver after the tests have completed.
     tearDownAll(() async {
       if (driver != null) {
-        driver.close();
+        await driver.close();
       }
     });
 
